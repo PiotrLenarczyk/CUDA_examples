9efd5b7fd80792ad95af2ec5c2196a21257d35bc
PiotrLenarczykAnonim <piotr.lenarczyk@wat.edu.pl>
2017-03-06 19:52:58 +0100

    small changes

--- 09_thrustVecTuple/vecTuple.cu
+++ 09_thrustVecTuple/vecTuple.cu
@@ -7,16 +7,20 @@
 #include <vector>
 
 int N = 10;
-thrust::tuple<int, const char *> tString( N, "thrust");
+thrust::tuple< int, const char * > tString( N, "thrust" );
 
 int main( void )
 {
-    std::cout << "The 1st value of tString is " << thrust::get<0>( tString ) << std::endl;
+    std::cout << "The 1st value of tString is " << thrust::get< 0 >( tString ) << std::endl;
     std::vector < float > vecStd( N, 0.1f );
     thrust::host_vector< float > h_vec( N );
     h_vec = vecStd;
     std::cout << "h_vec from vecStd :" << std::endl;
     thrust::copy( h_vec.begin(), h_vec.end(), std::ostream_iterator< float >( std::cout, "\n" ) );
-
+    
+    thrust::device_vector< float > d_vec( N );
+    d_vec = vecStd;
+    std::cout << "d_vec from vecStd :" << std::endl;
+    thrust::copy( d_vec.begin(), d_vec.end(), std::ostream_iterator< float >( std::cout, "\n" ) );
     return 0;
 }
--- 10_thrust_VecTHD/vecTHD.cu
+++ 10_thrust_VecTHD/vecTHD.cu
@@ -14,7 +14,7 @@
 typedef thrust::tuple< float, float, float, float > vec4;
 
 // return a random vec4 in [0,1)^2
-vec4 make_random_vec4(void)
+vec4 make_random_vec4( void )
 {
   static thrust::default_random_engine rng;
   static thrust::uniform_real_distribution< float > u01( 0.0f, 1.0f );
@@ -29,8 +29,8 @@ int main ( void )
 {
     const size_t N = 1000000;
     // allocate some random points on the host
-    thrust::host_vector<vec4> h_5Dpoints(N);
-    thrust::generate(h_5Dpoints.begin(), h_5Dpoints.end(), make_random_vec4);
+    thrust::host_vector<vec4> h_5Dpoints( N );
+    thrust::generate( h_5Dpoints.begin(), h_5Dpoints.end(), make_random_vec4 );
     std::cout << "The x4D[ 0 ] of h_5Dpoints is " << thrust::get< 3 >( h_5Dpoints[0] ) << std::endl;
     
     // transfer to device
