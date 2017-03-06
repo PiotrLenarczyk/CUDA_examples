4469fb3e0e13072037d32d4671935f464da92c96
PiotrLenarczyk <p.lenarczyk@ibles.waw.pl>
2017-03-06 07:18:42 +0100

    small speeling

--- 05_GPU_APP_ANALYSIS/RUN_GPU_APP_ANALYSIS_sudo.sh
+++ 05_GPU_APP_ANALYSIS/RUN_GPU_APP_ANALYSIS_sudo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#prpfiling for analysis: trivial Vector Sum
+#profiling for analysis: trivial Vector Sum
 echo "======================================================"
 make -j `nproc` -C ../03_trivialVectorSum
 sudo nvprof ../03_trivialVectorSum/a.out
@@ -7,7 +7,7 @@ make clean -C ../03_trivialVectorSum && make delete -C ../03_trivialVectorSum
 echo "======================================================"
 echo ""
 echo "======================================================"
-#prpfiling for analysis: basic asynchronous optimalization for Vector Sum
+#profiling for analysis: basic asynchronous optimalization for Vector Sum
 make -j `nproc` -C ../04_asyncOptimalization
 sudo nvprof ../04_asyncOptimalization/a.out
 make clean -C ../04_asyncOptimalization && make delete -C ../04_asyncOptimalization
