46cb8a74c01a1e41476a75d3eb9ba9bed7bf8083
PiotrLenarczykAnonim <piotr.lenarczyk@wat.edu.pl>
2017-03-15 21:50:06 +0100

    dynamic Parallelism hello

---
+++ 12_dynPar_Hello/RUN_COMMANDS.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#also one can run: 
+#                   make && ./a.out
+make delete
+make -j`nproc`
+make clean
+clear 
+./a.out
---
+++ 12_dynPar_Hello/dynPar.cu
@@ -0,0 +1,23 @@
+//STL
+#include <stdio.h>
+//CUDA
+//#include "../book.h"
+
+__global__ void childKernel()
+{
+    printf( "Hello %d", threadIdx.x );
+}
+
+__global__ void parentKernel()
+{
+    childKernel<<< 1, 2 >>>();
+    cudaDeviceSynchronize();
+    printf( "World!\n" );
+}
+
+int main( int argc, char *argv[] )
+{
+    parentKernel<<< 1, 2 >>>();
+    cudaDeviceSynchronize();
+    return 0;
+}
