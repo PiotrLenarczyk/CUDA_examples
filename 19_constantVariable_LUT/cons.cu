f2ae0f68570b3d032692f83889aeab6ac829e158
PiotrLenarczyk <p.lenarczyk@ibles.waw.pl>
2017-03-22 09:17:40 +0100

    device constant LUT view

--- 19_constantVariable_LUT/cons.cu
+++ 19_constantVariable_LUT/cons.cu
@@ -1,46 +1,55 @@
 //  http://cuda-programming.blogspot.com/2013/01/what-is-constant-memory-in-cuda.html
- //declare constant memory
-__constant__ float cangle[360]; //LUT candidate
+//STL
+#include <iostream>
 
-__global__ void test_kernel(float* darray);
+__constant__ float d_angle[ 360 ]; //constant memory LUT candidate
 
-int main(int argc,char** argv)
+__global__ void test_kernel( float* d_array );
+
+int main( int argc, char** argv )
 {
-         int size=3200;
-         float* darray;
-         float hangle[360];
+    unsigned size = 3200;
+    float* d_array;
+    float h_angle[ 360 ];
 
-          //allocate device memory
-         cudaMalloc ((void**)&darray,sizeof(float)*size);
-         
-         //initialize allocated memory
-        cudaMemset (darray,0,sizeof(float)*size);
+    //allocate device memory
+    cudaMalloc( ( void** ) &d_array, size * sizeof( float ) );
 
-         //initialize angle array on host
-       for(int loop=0;loop<360;loop++)
-                    hangle[loop] = acos( -1.0f )* loop/ 180.0f;
+    //initialize allocated memory
+    cudaMemset( d_array, 0, sizeof( float ) * size );
 
-        //copy host angle data to constant memory
-       cudaMemcpyToSymbol    (  cangle,  hangle,   sizeof(float)*360  );
+    //initialize angle array on host
+    for( unsigned loop=0; loop < 360; loop++ )
+        h_angle[ loop ] = acos( -1.0f ) * loop/ 180.0f;
+
+    //copy host angle data to constant memory
+    cudaMemcpyToSymbol( d_angle, h_angle, 360 * sizeof( float ) );
    
-        test_kernel  <<<  size/64  ,64  >>>  (darray);
-     
-       //free device memory
-       cudaFree(darray);
-  return 0;
+    test_kernel<<< size / 64, 64>>>( d_array );
+
+    //constant variable view
+    float DtH_angle[ 360 ]; cudaMemcpyFromSymbol( DtH_angle, d_angle, 360 * sizeof( float ) );
+        for ( unsigned i = 0; i < 360; i++ )
+    {
+        printf( "[ind=%02i]: h_angle: [ %.2f ]; DtH_angle: [ %.2f ] \n",
+                i,
+                h_angle[ i ],
+                DtH_angle[ i ]
+              );
+    }
+    
+    //free device memory
+    cudaFree( d_array );
+    cudaFree( d_angle );
+    return 0;
 }
 
-
-__global__ void test_kernel(float* darray)
+__global__ void test_kernel(float* d_array)
 {
-      int index;
-    
-      //calculate each thread global index
-     index = blockIdx.x * blockDim.x + threadIdx.x;
-   
+    //calculate each thread global index
+    unsigned index = blockIdx.x * blockDim.x + threadIdx.x;
     #pragma unroll 10
-   for(int loop=0;loop<360;loop++)
-                 darray[index]= darray [index] + cangle [loop] ;
-return;
-
+    for( unsigned loop=0; loop < 360; loop++ )
+        d_array[ index ] = d_array[ index ] + d_angle[ loop ];
+    return;
 }
