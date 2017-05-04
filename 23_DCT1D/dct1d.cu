becd530e4f7f0cd22c407b2de00888c1375952fc
PiotrLenarczykAnonim <piotr.lenarczyk@wat.edu.pl>
2017-05-04 14:05:21 +0200

    accuracy or precision

--- 23_DCT1D/dct1d.cu
+++ 23_DCT1D/dct1d.cu
@@ -12,7 +12,7 @@ vector < float > inputVec( N );
 string letter, subFp; const string sep( "_" );
 
 //===========================	gpu ===========================
-__device__      float d_x[ N ], d_Xfp32[ N ];
+__device__      float d_x[ N ], d_Xfp32[ N ], d_ix[ N ];
 __device__      double d_Xfp64[ N ]; 	//double size per dimension in comparision to floats array in global memory; for 2D results in quadratic size
 __constant__    unsigned d_N[ 1 ];
 __global__ void printKernel()
@@ -21,11 +21,29 @@ __global__ void printKernel()
         printf( "x[%i]: %f\n", i, d_x[ i ] );
     for ( unsigned i = 0; i < 5; i++ )
         printf( "d_Xfp32[%i]: %.6f;   d_Xfp64[%i]: %.6f\n", i, d_Xfp32[ i ], i, d_Xfp64[ i ] );
+	for ( unsigned i = 0; i < 5; i++ )
+        printf( "ix[%i]: %f\n", i, d_ix[ i ] );
     double acc = 0.0f;
     for( unsigned i = 0; i < N; i++ )
         acc += ( d_Xfp32[ i ] - d_Xfp64[ i ] ) * ( d_Xfp32[ i ] - d_Xfp64[ i ] );
     acc /= N;
-    printf( "mean difference in float vs double accumulators: %.6f\n", sqrtf( acc ) );
+    printf( "mean difference in dct float vs double accumulators: %.6f\n", sqrtf( acc ) );
+}
+
+__global__ void idctKernelFloat()
+{
+    unsigned ind = blockIdx.x * blockDim.x + threadIdx.x;
+    float constVal = float( ind ) * 3.14159265f / float( N );
+    float sqrConst = sqrtf( 2.0f / float( N ) );
+    float tmpX = sqrtf( 1.0f / float( N ) ) * d_Xfp32[ 0 ];
+    float accDC = 0.0f, tmpx = 0.0f; 
+    for ( unsigned i = 1; i < N; i++ )   
+    {
+        tmpx = d_Xfp32[ i ];
+        tmpX += tmpx * sqrConst * __cosf( constVal * ( float( i ) + 0.5f ) );
+        accDC += tmpx;
+    }
+    d_ix[ ind ] = tmpX;
 }
 
 __global__ void dctKernelFloat()
@@ -33,8 +51,7 @@ __global__ void dctKernelFloat()
     unsigned ind = blockIdx.x * blockDim.x + threadIdx.x;
     float constVal = float( ind ) * 3.14159265f / float( N );
     float sqrConst = sqrtf( 2.0f / float( N ) );
-    float tmpX = 0.0f;
-    float accDC = 0.0f; float tmpx = 0.0f;
+    float tmpX = 0.0f, accDC = 0.0f, tmpx = 0.0f;
     for ( unsigned i = 0; i < N; i++ )   
     {
         tmpx = d_x[ i ];
@@ -50,8 +67,7 @@ __global__ void dctKernelDouble()
     unsigned ind = blockIdx.x * blockDim.x + threadIdx.x;
     float constVal = float( ind ) * 3.14159265f / float( N );
     float sqrConst = sqrtf( 2.0f / float( N ) );
-    double tmpX = 0.0f;
-    double accDC = 0.0f; float tmpx = 0.0f;
+    double tmpX = 0.0f, accDC = 0.0f, tmpx = 0.0f;
     for ( unsigned i = 0; i < N; i++ )   
     {
         tmpx = d_x[ i ];
@@ -76,9 +92,15 @@ int main( int argc, char* argv[] )
     dctKernelDouble<<< N/256, 256 >>>();
     cudaDeviceSynchronize();
     cout << "CPU clocks double accumulator: " << double( clock() - t ) << endl;
+    
+    t = clock();
+    idctKernelFloat<<< N/256, 256 >>>();
+    cudaDeviceSynchronize();
+    cout << "CPU clocks idct float accumulator: " << double( clock() - t ) << endl;
     printKernel<<< 1, 1 >>>();
     
 	cudaFree( d_x );
+    cudaFree( d_ix );
     cudaFree( d_Xfp32 );
     cudaFree( d_Xfp64 );
     cudaFree( d_N );
