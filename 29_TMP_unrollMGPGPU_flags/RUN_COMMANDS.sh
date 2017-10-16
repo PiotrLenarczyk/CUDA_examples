42dbd2da910949d21d5f929f9a3b75092c0906f3
PiotrLenarczykAnonim <piotr.lenarczyk@wat.edu.pl>
2017-10-16 20:53:14 +0200

    CUDA kernel loop unrolling finished

--- 29_TMP_unrollMGPGPU_flags/RUN_COMMANDS.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-make delete && make -j`nproc` && make clean && clear && for (( i=0; i<2; i++ ));do echo "===="; echo "program run no. [$i]"; ./a.out; echo "====";done && touch a.out && rm a.out
--- 29_unrollMGPGPU_flags/mgpgpu.cu
+++ 29_unrollMGPGPU_flags/mgpgpu.cu
@@ -16,21 +16,55 @@ const uint nThreads = 512;
 const uint nBlocks = ( N / nThreads ) + 1;
 const uint UNROLLING = 16;                   //check [ 8 16 32 64 ]; I would guess sixteen times unrolling;
 __global__ void nop(){}; 
-#define cudaMemcpyHostToDevice H2D;          //useful alias
-#define cudaMemcpyDeviceToHost D2H;
+
 __global__ void trivialLoop()
 {
     uint a = 0;
     for ( uint32_t i = 0; i < N; i++ )
+    {
         a = i;
-}
+        a++;
+    }
+};
 
 __global__ void unrollTrivialLoop()
 {
     uint a = 0;
     #pragma unroll UNROLLING                //briliant feature
     for ( uint32_t i = 0; i < N; i++ )
+    {
         a = i;
+        a++;
+    }
+};
+
+__global__ void initializeDevice( float *d_in, const uint streamSize )
+{
+    uint tid = threadIdx.x;
+    uint idx = blockIdx.x * blockDim.x + tid;
+    if( idx < streamSize )
+    {
+        d_in[ idx ] *= 0.9f;
+    }
+};
+
+__global__ void loop( float *d_in, const uint streamNo, const uint streamSize )
+{
+    size_t ii = 0;
+    for ( ii = 0; ii < streamSize; ii++ )
+    {
+        d_in[ ii ] +=  float( ii );
+    }
+};
+
+__global__ void unrollLoop( float *d_in, const uint streamNo, const uint streamSize )
+{
+    size_t ii = 0;
+    #pragma unroll UNROLLING            //adjustable mainly to register-only, high performance computations - described at unrollTrivialLoop device kernel
+    for ( ii = 0; ii < streamSize; ii++ )
+    {
+        d_in[ ii ] +=  float( ii );
+    }
 };
 
 int main( void )
@@ -38,7 +72,8 @@ int main( void )
     cudaGetDeviceCount( &gpuCount );
     //HOST
     float *h_arr[ gpuCount ];           //     float **h_arr = ( float** )malloc( sizeof( float * ) * gpuCount ); //alternatively
-    uint perDevN = 1E3 / gpuCount;
+    uint perDevN = N / gpuCount;
+    uint perDevNBytes = sizeof( float ) * perDevN ;
     //DEVICE
     cudaStream_t stream[ gpuCount ];
     float *d_arr[ gpuCount ];           //     float **d_arr = ( float** )malloc( sizeof( float * ) * gpuCount ); //alternatively
@@ -47,11 +82,11 @@ int main( void )
     for ( i = 0; i < gpuCount; i++ )
     {
         //HOST 
-        cudaMallocHost( ( void** ) &h_arr[ i ], perDevN );
+        cudaMallocHost( ( void** ) &h_arr[ i ], perDevNBytes );
         initalizeHost( h_arr[ i ], perDevN );
         //DEVICE
         cudaSetDevice( i );
-        cudaMalloc( ( void** ) &d_arr[ i ], perDevN );
+        cudaMalloc( ( void** ) &d_arr[ i ], perDevNBytes );
         cudaStreamCreate( &stream[ i ] );
     }
     
@@ -66,32 +101,32 @@ int main( void )
         nop<<< 1, 1 >>>();
         auto t2 = chrono::high_resolution_clock::now();
         uint elapsed = uint( chrono::duration_cast< chrono::nanoseconds >( t2 - t1 ).count() ); 
-        printf( "loop elapsed: %d \n", elapsed );
+        printf( "trivial loop elapsed: %d \n", elapsed );
         
         t1 = chrono::high_resolution_clock::now();
         unrollTrivialLoop<<< 1, 1 >>>();
         nop<<< 1, 1 >>>();
         t2 = chrono::high_resolution_clock::now();
         elapsed = chrono::duration_cast< chrono::nanoseconds >( t2 - t1 ).count();  
+        printf( "trivial unrolled loop elapsed: %d \n", elapsed );
+        
+        
+        cudaMemcpyAsync( d_arr[ i ], h_arr[ i ], perDevNBytes, cudaMemcpyHostToDevice, stream[ i ] );
+        initializeDevice<<< nBlocks, nThreads, 0, stream[ i ] >>>( d_arr[ i ], perDevN );
+        nop<<< 1, 1 >>>();
+        t1 = chrono::high_resolution_clock::now();
+        loop<<< 1, 1, 0, stream[ i ] >>>( d_arr[ i ], i, perDevN );
+        nop<<< 1, 1 >>>();
+        t2 = chrono::high_resolution_clock::now();
+        elapsed = chrono::duration_cast< chrono::nanoseconds >( t2 - t1 ).count();  
+        printf( "loop elapsed: %d \n", elapsed );
+        
+        t1 = chrono::high_resolution_clock::now();
+        unrollLoop<<< 1, 1, 0, stream[ i ] >>>( d_arr[ i ], i, perDevN );
+        nop<<< 1, 1 >>>();
+        t2 = chrono::high_resolution_clock::now();
+        elapsed = chrono::duration_cast< chrono::nanoseconds >( t2 - t1 ).count();  
         printf( "unrolled loop elapsed: %d \n", elapsed );
-        
-        
-        
-        
-        
-        //cudaMemcpyAsync( d_arr[i], h_arr[i], H2D, stream[i] );
-        //kernel<<< nT, nB, stream[i] >>>( d_arr, perDevN );
-        /*    uint tid = threadIdx.x;
-              uint idx = blockIdx.x * blockDim.x + threadIdx.x;
-              if( idx < perDevN ) :...
-        */
-        //cudaMemcpyAsync( gpuRef[i], d_arr, D2H stream[i] );
-        
-        
-        
-        
-        
-        
     }
     
     //free memories
@@ -112,7 +147,8 @@ int main( void )
 void initalizeHost( float *ip, uint size )
 {
     for ( size_t i = 0; i < size; i++ )
-        ip[ i ] = 0.0f;
+        ip[ i ] = 1.2f;
 };
 
 //Post Scriptum: In my professional opinion, coprocessors: GTX1080ti is brand-new and off-the-shell optimal; GTX770 is used optimal - I've heard about R9Nano and HD5770 ( GFLOPS/USD; GFLOPS/W; QualityWithBandwidthAndMemSize/Price; ); 
+//Post Post Scriptum: I do strongly recommend profiling with NVidia's NVPROF profiling tool, instead of CPU high-resolution timer.
