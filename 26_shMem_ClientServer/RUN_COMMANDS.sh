3a33cc5d2f01bf69c62918dfe3b89b0b82f98721
Piotr Lenarczyk <piotrl@elprotronic.com>
2017-11-14 11:36:53 +0100

    privately holded devices update

--- 26_shMem_ClientServer/RUN_COMMANDS.sh
+++ 26_shMem_ClientServer/RUN_COMMANDS.sh
@@ -2,7 +2,7 @@
 #communication is unidirectional! from clients to listening server
 touch shMemSrv && rm shMemSrv && touch gpuClient && rm gpuClient && touch printShMem && rm printShMem
 g++ serverShMem.cpp -o shMemSrv 
-`find /usr -type f -name 'nvcc'` -arch=sm_35 -std=c++11 gpuClient.cu -o gpuClient
+`find /usr -type f -name 'nvcc'` -gencode=arch=compute_35,code=sm_35 -gencode=arch=compute_61,code=sm_61 -rdc=true --fmad=true -std=c++11 gpuClient.cu -o gpuClient
 g++ printSERVERShMem.cpp -o printShMem 
 ./shMemSrv 5600 #now server is listening on port 5600 with SERVER_SHMID
 #at new terminal send shared memory struct from client to server: #./gpuClient localhost 5600
